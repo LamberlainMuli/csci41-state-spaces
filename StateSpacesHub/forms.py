e14b899e3d67925f8090bc704b70d217331bfb93
Michelle Abarico <michelleabarico@Michelles-MacBook-Pro.local>
2024-11-27 20:03:53 +0800

    commented other entities in forms

--- StateSpacesHub/forms.py
+++ StateSpacesHub/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from .models import Building, Venue, Amenity
 
+# Agent,
+# TeamMember,
+# Customer,
+# Reservation,
+# Renovation
+        
+
 class BuildingForm(forms.ModelForm):
     class Meta:
         model = Building
@@ -13,4 +20,23 @@ class AmenityForm(forms.ModelForm):
     class Meta:
         model = Amenity
         exclude=[]
-        
+# class AgentForm(forms.ModelForm):
+#     class Meta:
+#         model = Amenity
+#         exclude=[]
+# class TeamMemberForm(forms.ModelForm):
+#     class Meta:
+#         model = TeamMember
+#         exclude=[]
+# class CustomerForm(forms.ModelForm):
+#     class Meta:
+#         model = Customer
+#         exclude=[]
+# class ReservationForm(forms.ModelForm):
+#     class Meta:
+#         model = Reservation
+#         exclude=[]
+# class RenovationForm(forms.ModelForm):
+#     class Meta:
+#         model = Renovation
+#         exclude=[]
