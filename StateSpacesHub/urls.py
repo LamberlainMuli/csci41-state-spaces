974114a6edb13c732874a5191fcffaf2afa9bbaa
Michelle Abarico <michelleabarico@Michelles-MacBook-Pro.local>
2024-11-27 20:34:36 +0800

    urls comments for other entities

--- StateSpacesHub/urls.py
+++ StateSpacesHub/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('building/add', BuildingCreateView.as_view(), name='building_add'),
     path('building/<int:pk>/edit', BuildingUpdateView.as_view(), name='building_edit'),
     
-    
     path('venue/list', VenueListView.as_view(), name='venue'),
     path('venue/<int:pk>', VenueDetailView.as_view(), name='venue_detail'),
     path('venue/add', VenueCreateView.as_view(), name='venue_add'),
@@ -18,6 +17,32 @@ urlpatterns = [
     path('amenity/<int:pk>', AmenityDetailView.as_view(), name='amenity_detail'),
     path('amenity/add', AmenityCreateView.as_view(), name='amenity_add'),
     path('amenity/<int:pk>/edit', AmenityUpdateView.as_view(), name='amenity_edit'),
+
+    # path('agent/list', AgentListView.as_view(), name='agent'),
+    # path('agent/<int:pk>', AgentDetailView.as_view(), name='agent_detail'),
+    # path('agent/add', AgentCreateView.as_view(), name='agent_add'),
+    # path('agent/<int:pk>/edit', AgentUpdateView.as_view(), name='agent_edit'),
+
+    # path('team_member/list', TeamMemberListView.as_view(), name='team_member'),
+    # path('team_member/<int:pk>', TeamMemberDetailView.as_view(), name='team_member_detail'),
+    # path('team_member/add', TeamMemberCreateView.as_view(), name='team_member_add'),
+    # path('team_member/<int:pk>/edit', TeamMemberUpdateView.as_view(), name='team_member_edit'),
+
+    # path('customer/list', CustomerListView.as_view(), name='customer'),
+    # path('customer/<int:pk>', CustomerDetailView.as_view(), name='customer_detail'),
+    # path('customer/add', CustomerCreateView.as_view(), name='customer_add'),
+    # path('customer/<int:pk>/edit', CustomerUpdateView.as_view(), name='customer_edit'),
+
+    # path('reservation/list', ReservationListView.as_view(), name='reservation'),
+    # path('reservation/<int:pk>', ReservationDetailView.as_view(), name='reservation_detail'),
+    # path('reservation/add', ReservationCreateView.as_view(), name='reservation_add'),
+    # path('reservation/<int:pk>/edit', ReservationUpdateView.as_view(), name='reservation_edit'),
+
+    # path('reservation/list', ReservationListView.as_view(), name='reservation'),
+    # path('reservation/<int:pk>', ReservationDetailView.as_view(), name='reservation_detail'),
+    # path('reservation/add', ReservationCreateView.as_view(), name='reservation_add'),
+    # path('reservation/<int:pk>/edit', ReservationUpdateView.as_view(), name='reservation_edit'),
+
 ]
 
 app_name = 'StateSpacesHub'
