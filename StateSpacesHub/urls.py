58fc1258d923065716dc09c0ddcfe6487c9d77e0
LamberlainMuli <109662687+LamberlainMuli@users.noreply.github.com>
2024-11-27 18:49:23 +0800

    Changed Camel Case to Title Case

--- StateSpacesHub/urls.py
+++ StateSpacesHub/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from .views import buildingListView, buildingDetailView, venueListView, venueDetailView, amenityListView, amenityDetailView
+from .views import BuildingListView, BuildingDetailView, VenueListView, VenueDetailView, AmenityListView, AmenityDetailView
 
 
 urlpatterns = [
-    path('building/list', buildingListView.as_view(), name='Building'),
-    path('building/<int:pk>', buildingDetailView.as_view(), name='building_detail'),
+    path('building/list', BuildingListView.as_view(), name='Building'),
+    path('building/<int:pk>', BuildingDetailView.as_view(), name='building_detail'),
 
-    path('venue/list', venueListView.as_view(), name='venue'),
-    path('venue/<int:pk>', venueDetailView.as_view(), name='venue_detail'),
+    path('venue/list', VenueListView.as_view(), name='venue'),
+    path('venue/<int:pk>', VenueDetailView.as_view(), name='venue_detail'),
 
-    path('amenity/list', amenityListView.as_view(), name='amenity'),
-    path('amenity/<int:pk>', amenityDetailView.as_view(), name='amenity_detail'),
-
+    path('amenity/list', AmenityListView.as_view(), name='amenity'),
+    path('amenity/<int:pk>', AmenityDetailView.as_view(), name='amenity_detail'),
 ]
 
 app_name = 'StateSpacesHub'
