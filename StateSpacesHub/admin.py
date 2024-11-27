dfcc98bbc7c93162ee17cf36abd73ea14f7ca7b8
LamberlainMuli <109662687+LamberlainMuli@users.noreply.github.com>
2024-11-27 18:24:50 +0800

    Added templates directory to settings.py

--- StateSpacesHub/admin.py
+++ StateSpacesHub/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Building, Venue, Amenity
 
-# Register your models here.
+
+class VenueInline(admin.TabularInline):
+    model = Venue
+    extra = 1  
+
+
+class AmenityInline(admin.TabularInline):
+    model = Amenity
+    extra = 1  
+
+
+class BuildingAdmin(admin.ModelAdmin):
+    model = Building
+    search_fields = ['building_name']  
+    list_display = ['id', 'building_name']  
+    inlines = [VenueInline]  
+
+
+class VenueAdmin(admin.ModelAdmin):
+    model = Venue
+    search_fields = ['venue_name', 'building__building_name']  
+    list_filter = ['venue_type', 'venue_status', 'building']  
+    list_display = ['id', 'venue_name', 'building', 'venue_type', 'venue_capacity', 'venue_status']
+    inlines = [AmenityInline]  
+
+
+class AmenityAdmin(admin.ModelAdmin):
+    model = Amenity
+    search_fields = ['amenity_type', 'venue__venue_name']  
+    list_filter = ['venue']  
+    list_display = ['id', 'amenity_type', 'venue', 'amenity_description']
+
+
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(Amenity, AmenityAdmin)
--- StateSpacesHub/views.py
+++ StateSpacesHub/views.py
@@ -1,34 +1,86 @@
-from django.shortcuts import render
-from .models import Building, Venue, Amenity
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Building, Venue, Amenity
+from .forms import BuildingForm, VenueForm, AmenityForm  # Create these forms in `forms.py`
 
-
-class buildingListView(ListView):
+# Building Views
+class BuildingListView(LoginRequiredMixin, ListView):
     model = Building
-    template_name = 'buildingList.html'
+    template_name = 'statespaceshub/building_list.html'
+    context_object_name = 'buildings'
 
 
-class buildingDetailView(DetailView):
+class BuildingDetailView(LoginRequiredMixin, DetailView):
     model = Building
-    template_name = 'buildingDetail.html'
+    template_name = 'statespaceshub/building_detail.html'
+    context_object_name = 'building'
 
 
-class venueListView(ListView):
+class BuildingCreateView(LoginRequiredMixin, CreateView):
+    model = Building
+    form_class = BuildingForm
+    template_name = 'statespaceshub/building_form.html'
+    success_url = reverse_lazy('StateSpacesHub:building-list')
+
+
+class BuildingUpdateView(LoginRequiredMixin, UpdateView):
+    model = Building
+    form_class = BuildingForm
+    template_name = 'statespaceshub/building_form.html'
+    success_url = reverse_lazy('StateSpacesHub:building-detail')
+
+# Venue Views
+class VenueListView(LoginRequiredMixin, ListView):
     model = Venue
-    template_name = 'venueList.html'
+    template_name = 'statespaceshub/venue_list.html'
+    context_object_name = 'venues'
 
 
-class venueDetailView(DetailView):
+class VenueDetailView(LoginRequiredMixin, DetailView):
     model = Venue
-    template_name = 'venueDetail.html'
+    template_name = 'statespaceshub/venue_detail.html'
+    context_object_name = 'venue'
 
 
-class amenityListView(ListView):
+class VenueCreateView(LoginRequiredMixin, CreateView):
+    model = Venue
+    form_class = VenueForm
+    template_name = 'statespaceshub/venue_form.html'
+    success_url = reverse_lazy('StateSpacesHub:venue-list')
+
+
+class VenueUpdateView(LoginRequiredMixin, UpdateView):
+    model = Venue
+    form_class = VenueForm
+    template_name = 'statespaceshub/venue_form.html'
+    success_url = reverse_lazy('StateSpacesHub:venue-detail')
+
+
+# Amenity Views
+class AmenityListView(LoginRequiredMixin, ListView):
     model = Amenity
-    template_name = 'amenityList.html'
+    template_name = 'statespaceshub/amenity_list.html'
+    context_object_name = 'amenities'
 
 
-class amenityDetailView(DetailView):
+class AmenityDetailView(LoginRequiredMixin, DetailView):
     model = Amenity
-    template_name = 'amenityDetail.html'
+    template_name = 'statespaceshub/amenity_detail.html'
+    context_object_name = 'amenity'
+
+
+class AmenityCreateView(LoginRequiredMixin, CreateView):
+    model = Amenity
+    form_class = AmenityForm
+    template_name = 'statespaceshub/amenity_form.html'
+    success_url = reverse_lazy('StateSpacesHub:amenity-list')
+
+
+class AmenityUpdateView(LoginRequiredMixin, UpdateView):
+    model = Amenity
+    form_class = AmenityForm
+    template_name = 'statespaceshub/amenity_form.html'
+    success_url = reverse_lazy('StateSpacesHub:amenity-detail')
