db8c949cdd443049213119207ee66b72365c5d07
CarylRaeChan <caryl.chan@student.ateneo.edu>
2024-11-27 17:24:19 +0800

    did the views.py

--- StateSpacesHub/views.py
+++ StateSpacesHub/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Building, Venue, Amenity
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+
+class buildingListView(ListView):
+    model = Building
+    template_name = 'buildingList.html'
+
+
+class buildingDetailView(DetailView):
+    model = Building
+    template_name = 'buildingDetail.html'
+
+
+class venueListView(ListView):
+    model = Venue
+    template_name = 'venueList.html'
+
+
+class venueDetailView(DetailView):
+    model = Venue
+    template_name = 'venueDetail.html'
+
+
+class amenityListView(ListView):
+    model = Amenity
+    template_name = 'amenityList.html'
+
+
+class amenityDetailView(DetailView):
+    model = Amenity
+    template_name = 'amenityDetail.html'
